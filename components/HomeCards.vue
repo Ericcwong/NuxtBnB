87e8a4a51e825ab477107101d61a08dea8134ebc
Eric Wong <ericwong12@live.com>
2021-01-22 13:52:46 -0700

    Initial Commit

---
+++ components/HomeCards.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="cards">
+    <img :src="home.images[0]" /><br />
+    <div class="description">
+      {{ home.title }} <br />
+      {{ home.location.address }} {{ home.location.city }}
+      {{ home.location.state }} <br />
+      {{ home.guests }} guests <br />
+      {{ home.pricePerNight }} / night <br />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    home: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+img {
+  width: 200px;
+  margin: 10px;
+}
+.description {
+  margin: 10px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="card" v-for="home in homes" :key="home.objectID">
+      <HomeCards :home="home" />
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from "~/data/homes.json";
+export default {
+  head() {
+    return {
+      title: "Home page",
+      meta: [
+        {
+          name: "description",
+          content: "This is a home page!",
+          hid: "description",
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
